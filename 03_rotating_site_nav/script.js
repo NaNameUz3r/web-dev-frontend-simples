e6da567c8a79660462526991abd2d636d1988f01
Ivan Zakutniy <ivan.zakutny@flant.com>
2023-07-03 10:40:37 +0400

    rotating nav
    Signed-off-by: Ivan Zakutniy <ivan.zakutny@flant.com>

--- 03_rotating_site_nav/script.js
+++ 03_rotating_site_nav/script.js
@@ -1,53 +1,6 @@
-const progress = document.getElementById('progress')
-const prevButton = document.getElementById('prev')
-const nextButton = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
-
-let currentActive = 1
-
-nextButton.addEventListener('click', () => {
-    console.log(currentActive)
-    if (currentActive >= circles.length) {
-        currentActive = circles.length
-    } else {
-        currentActive++
-    }
-
-    update()
-})
-
-prevButton.addEventListener('click', () => {
-    console.log(currentActive)
-    if (currentActive <= 1) {
-        currentActive = 1
-    } else {
-        currentActive--
-    }
-
-    update()
-})
-
-
-function update() {
-    circles.forEach((circle, i) => {
-        if (i < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = ((actives.length - 1)/ (circles.length -1)) * 100 + '%'
-
-    if(currentActive === 1) {
-        prevButton.disabled = true
-    } else if(currentActive === circles.length) {
-        nextButton.disabled = true
-    } else {
-        prevButton.disabled = false
-        nextButton.disabled = false
-    }
-}
+open.addEventListener('click', () => container.classList.add("show-nav"))
+close.addEventListener('click', () => container.classList.remove("show-nav"))
